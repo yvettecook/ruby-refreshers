3f46079c488c9c124efa661be56ef4ab15c5dd7b
yvettecook <yvettecook@gmail.com>
2014-10-31 14:29:55 +0000

    Test 29: Getting domain name from email

--- questions.rb
+++ questions.rb
@@ -196,6 +196,8 @@ end
 # get the domain name *without* the .com part, from an email address
 # so alex@makersacademy.com becomes makersacademy
 def get_domain_name_from_email_address(email)
+	step1 = email.partition("@")[2]
+	step2 = step1.partition(".")[0]
 end
 
 # capitalize the first letter in each word of a string, 
--- questions_spec.rb
+++ questions_spec.rb
@@ -152,7 +152,7 @@ describe 'the Friday test :)' do
     expect(n).to eq '31/10/2013'
   end
 
-  specify 'get_domain_name_from_email_address' do
+  fspecify 'get_domain_name_from_email_address' do
     n = get_domain_name_from_email_address 'alex@makersacademy.com'
     expect(n).to eq 'makersacademy'
   end
