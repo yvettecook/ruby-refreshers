43bf8df58f6b8538244d990e03e958c948c89618
yvettecook <yvettecook@gmail.com>
2014-10-31 13:54:54 +0000

    Test 23: Nice and quick

--- questions_spec.rb
+++ questions_spec.rb
@@ -122,7 +122,7 @@ describe 'the Friday test :)' do
     expect(n).to eq ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
   end
 
-  specify 'swap_keys_and_values_in_a_hash' do
+  fspecify 'swap_keys_and_values_in_a_hash' do
     n = swap_keys_and_values_in_a_hash({'a' => 'b', 'c' => 'd'})
     expect(n).to eq({'b' => 'a', 'd' => 'c'})
   end
